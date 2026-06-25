796c11a35d5630a84c028af74dc54a33995855b3
Salman Najah <salmannajah123@gmail.com>
2026-06-25 13:39:53 +0530

    validations and modifying pvt variables using setter methods

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -45,6 +45,33 @@ impl Ticket {
     pub fn status(&self) -> &String {
         &self.status
     }
+
+    pub fn set_title(&mut self, new_title: String) {
+        self.title = new_title;
+        if self.title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if self.title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+    }
+
+    pub fn set_description(&mut self, new_description: String) {
+        self.description = new_description;
+        if self.description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if self.description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+    }
+
+    pub fn set_status(&mut self, new_status: String) {
+        self.status = new_status;
+        if self.status != "To-Do" && self.status != "In Progress" && self.status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+    }
 }
 
 #[cfg(test)]
